096710ddeb8dd33930a624c653047ab54cf633bf
Scott2444 <scotty.haakenson@gmail.com>
2025-12-03 13:52:09 -0500

    PublicID column script fixed

--- database/migrations/U5__puid_column.sql
+++ database/migrations/U5__puid_column.sql
@@ -1,5 +1,7 @@
 ALTER TABLE app.users
     DROP COLUMN puid;
+DROP INDEX IF EXISTS idx_users_puid;
 
 ALTER TABLE app.projects
     DROP COLUMN puid;
+DROP INDEX IF EXISTS idx_projects_puid;
--- database/migrations/V5__puid_column.sql
+++ database/migrations/V5__puid_column.sql
@@ -1,7 +1,7 @@
 ALTER TABLE app.users
     ADD COLUMN puid CHAR(10) UNIQUE NOT NULL;
-    ADD UNIQUE INDEX idx_users_puid (puid);
+CREATE UNIQUE INDEX idx_users_puid ON app.users (puid);
 
 ALTER TABLE app.projects
     ADD COLUMN puid CHAR(10) UNIQUE NOT NULL;
-    ADD UNIQUE INDEX idx_projects_puid (puid);
+CREATE UNIQUE INDEX idx_projects_puid ON app.projects (puid);
