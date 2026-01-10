33fb6b59061ee5dd8efd2b532c212a6325d65ca9
Scott2444 <scotty.haakenson@gmail.com>
2026-01-10 01:18:35 -0500

    Alias puid instead of id

---
+++ database/migrations/V8__public_project2.sql
@@ -0,0 +1,9 @@
+alter table app.projects
+    drop column alias_project_id;
+
+alter table app.projects
+    add column alias_project_puid char(10)
+    default null
+        constraint fk_alias_project
+            references app.projects(project_puid)
+            on delete set null;
