60c5720cb8ab440d7128804e1caba38746f725db
Scott2444 <scotty.haakenson@gmail.com>
2026-01-10 01:27:32 -0500

    Alias puid instead of id again

--- database/migrations/V8__public_project2.sql
+++ database/migrations/V8__public_project2.sql
@@ -5,5 +5,5 @@ alter table app.projects
     add column alias_project_puid char(10)
     default null
         constraint fk_alias_project
-            references app.projects(project_puid)
+            references app.projects(puid)
             on delete set null;
