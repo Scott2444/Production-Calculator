77e3c204c8ca06b7c13ef9e545683ad6d6cc16d9
Scott2444 <scotty.haakenson@gmail.com>
2026-02-20 00:30:53 -0500

    Normal console logs when running locally

--- backend/ProductionCalculator.API/Helpers/LogConfig.cs
+++ backend/ProductionCalculator.API/Helpers/LogConfig.cs
@@ -19,8 +19,18 @@ namespace ProductionCalculator.API.Helpers
                 .MinimumLevel.Override("Microsoft.AspNetCore.Mvc.Infrastructure.ControllerActionInvoker", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.AspNetCore.Mvc.Infrastructure.ObjectResultExecutor", LogEventLevel.Warning)
                 .Enrich.FromLogContext()
-                .Enrich.WithProperty("Environment", env)
-                .WriteTo.Console(new JsonFormatter());
+                .Enrich.WithProperty("Environment", env);
+
+            var isDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+            if (isDocker)
+            {
+                loggerConfig.WriteTo.Console(new JsonFormatter());
+            }
+            else
+            {
+                loggerConfig.WriteTo.Console(
+                    outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}");
+            }
 
             Log.Logger = loggerConfig.CreateLogger();
             builder.Host.UseSerilog();
